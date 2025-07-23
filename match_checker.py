dcd599f7b36921d67834cb3569e262bc7212e9e3
fuglybynature <albe4life@gmail.com>
2025-07-23 09:03:28 +0300

    Update match_checker.py

--- match_checker.py
+++ match_checker.py
@@ -1,15 +1,16 @@
 # match_checker.py
 
+import os
 import requests
 
-API_KEY = "123"  # Твій безкоштовний API key TheSportsDB
+API_KEY = os.getenv("SPORTSDB_API_KEY")  # з .env
 
 def get_next_matches(team_name):
     # Пошук команди по назві
     url_search = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/searchteams.php?t={team_name}"
     resp = requests.get(url_search)
     data = resp.json()
-    if not data['teams']:
+    if not data.get('teams'):
         return []
 
     team_id = data['teams'][0]['idTeam']
