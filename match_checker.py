ecc36255187f01d15f4662b87e4379a2684c0310
fuglybynature <albe4life@gmail.com>
2025-07-23 15:55:53 +0300

    Update match_checker.py

--- match_checker.py
+++ match_checker.py
@@ -3,7 +3,7 @@
 import os
 import requests
 
-API_KEY = os.getenv("SPORTSDB_API_KEY")  # з .env
+API_KEY = os.getenv("API_KEY")  # з .env
 
 def get_next_matches(team_name):
     # Пошук команди по назві
